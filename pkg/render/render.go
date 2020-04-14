5e4b6693e54a27f58af5da2dd7db87454e0d7ea2
Ben Nemec <bnemec@redhat.com>
2020-04-14 12:18:16 -0500

    DNM: Test ci on bad patch

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,10 +1,8 @@
 package render
 
 import (
-	"fmt"
-	"io/ioutil"
+	"errors"
 	"os"
-	"path"
 	"text/template"
 
 	"github.com/sirupsen/logrus"
@@ -41,47 +39,5 @@ func RenderFile(renderPath, templatePath string, cfg interface{}) error {
 }
 
 func Render(outDir string, paths []string, cfg interface{}) error {
-	tempPaths := paths
-	if len(paths) == 1 {
-		fi, err := os.Stat(paths[0])
-		if err != nil {
-			log.WithFields(logrus.Fields{
-				"path": paths[0],
-			}).Error("Failed to stat file")
-		}
-		if fi.Mode().IsDir() {
-			templateDir := paths[0]
-			files, err := ioutil.ReadDir(templateDir)
-			if err != nil {
-				log.WithFields(logrus.Fields{
-					"path": templateDir,
-				}).Error("Failed to read template directory")
-				return err
-			}
-			tempPaths = make([]string, 0)
-			for _, entryFi := range files {
-				if entryFi.Mode().IsRegular() {
-					if path.Ext(entryFi.Name()) == ext {
-						tempPaths = append(tempPaths, path.Join(templateDir, entryFi.Name()))
-					}
-				}
-			}
-		}
-	}
-	for _, templatePath := range tempPaths {
-		if path.Ext(templatePath) != ext {
-			return fmt.Errorf("Template %s does not have the right extension. Must be '%s'", templatePath, ext)
-		}
-
-		baseName := path.Base(templatePath)
-		renderPath := path.Join(outDir, baseName[:len(baseName)-extLen])
-		err := RenderFile(renderPath, templatePath, cfg)
-		if err != nil {
-			log.WithFields(logrus.Fields{
-				"path": templatePath,
-				"err":  err,
-			}).Error("Failed to render template")
-		}
-	}
-	return nil
+	return errors.New("Test error")
 }
