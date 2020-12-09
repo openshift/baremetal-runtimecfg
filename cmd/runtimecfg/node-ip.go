openshift/baremetal-runtimecfg

commit 83d05afd1b76b8f0078887f1001e722284235a3a

runtimecfg node-ip: set KUBELET_NODE_IPS in addition to KUBELET_NODE_IP
When "runtimecfg node-ip" is called on a node with dual-stack IPs, set
"KUBELET_NODE_IPS=<primary-ip>,<secondary-ip>" (in addition to still setting
KUBELET_NODE_IP to the primary IP).

On single-stack nodes, we also set KUBELET_NODE_IPS, but it's
identical to KUBELET_NODE_IP. (Doing things this way makes it easier
to consume the output from a systemd service file in MCO.)

cmd/runtimecfg/node-ip.go
@@ -68,13 +68,13 @@ func show(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	chosenAddress, err := getSuitableIP(retry, vips)
+	chosenAddresses, err := getSuitableIPs(retry, vips)
 	if err != nil {
 		return err
 	}
-	log.Infof("Chosen Node IP %s", chosenAddress)
+	log.Infof("Chosen Node IPs: %v", chosenAddresses)
 
-	fmt.Println(chosenAddress)
+	fmt.Println(chosenAddresses[0])
 	return nil
 }
 
@@ -84,11 +84,11 @@ func set(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	chosenAddress, err := getSuitableIP(retry, vips)
+	chosenAddresses, err := getSuitableIPs(retry, vips)
 	if err != nil {
 		return err
 	}
-	log.Infof("Chosen Node IP %s", chosenAddress)
+	log.Infof("Chosen Node IPs: %v", chosenAddresses)
 
 	// Kubelet
 	kubeletOverrideDir := filepath.Dir(kubeletSvcOverridePath)
@@ -103,7 +103,12 @@ func set(cmd *cobra.Command, args []string) error {
 	}
 	defer kOverride.Close()
 
-	kOverrideContent := fmt.Sprintf("[Service]\nEnvironment=\"KUBELET_NODE_IP=%s\"\n", chosenAddress)
+	nodeIP := chosenAddresses[0].String()
+	nodeIPs := nodeIP
+	if len(chosenAddresses) > 1 {
+		nodeIPs += "," + chosenAddresses[1].String()
+	}
+	kOverrideContent := fmt.Sprintf("[Service]\nEnvironment=\"KUBELET_NODE_IP=%s\" \"KUBELET_NODE_IPS=%s\"\n", nodeIP, nodeIPs)
 	log.Infof("Writing Kubelet service override with content %s", kOverrideContent)
 	_, err = kOverride.WriteString(kOverrideContent)
 	if err != nil {
@@ -123,7 +128,7 @@ func set(cmd *cobra.Command, args []string) error {
 	}
 	defer cOverride.Close()
 
-	cOverrideContent := fmt.Sprintf("[Service]\nEnvironment=\"CONTAINER_STREAM_ADDRESS=%s\"\n", chosenAddress)
+	cOverrideContent := fmt.Sprintf("[Service]\nEnvironment=\"CONTAINER_STREAM_ADDRESS=%s\"\n", chosenAddresses[0])
 	log.Infof("Writing CRI-O service override with content %s", cOverrideContent)
 	_, err = cOverride.WriteString(cOverrideContent)
 	if err != nil {
@@ -132,26 +137,18 @@ func set(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func getSuitableIP(retry bool, vips []net.IP) (chosen net.IP, err error) {
+func getSuitableIPs(retry bool, vips []net.IP) (chosen []net.IP, err error) {
 	for {
-		var nodeAddrs []net.IP
-		var err error
-
 		if len(vips) > 0 {
-			nodeAddrs, err = utils.AddressesRouting(vips, utils.ValidNodeAddress)
-			if err != nil {
-				return nil, err
+			chosen, err = utils.AddressesRouting(vips, utils.ValidNodeAddress)
+			if len(chosen) > 0 || err != nil {
+				return chosen, err
 			}
-		}
-		if len(nodeAddrs) == 0 {
-			nodeAddrs, err = utils.AddressesDefault(utils.ValidNodeAddress)
-			if err != nil {
-				return nil, err
+		} else {
+			chosen, err = utils.AddressesDefault(utils.ValidNodeAddress)
+			if len(chosen) > 0 || err != nil {
+				return chosen, err
 			}
-		}
-		if len(nodeAddrs) > 0 {
-			chosen = nodeAddrs[0]
-			break
 		}
 		if !retry {
 			return nil, fmt.Errorf("Failed to find node IP")
@@ -160,7 +157,6 @@ func getSuitableIP(retry bool, vips []net.IP) (chosen net.IP, err error) {
 		log.Errorf("Failed to find a suitable node IP")
 		time.Sleep(time.Second)
 	}
-	return
 }
 
 func parseIPs(args []string) ([]net.IP, error) {

pkg/utils/addresses.go
@@ -114,7 +114,11 @@ func usableIPv6Route(route netlink.Route) bool {
 	return true
 }
 
-// AddressesRouting takes a slice of Virtual IPs and returns a slice of configured addresses in the current network namespace that directly route to those vips. You can optionally pass an AddressFilter to further filter down which addresses are considered
+func isIPv6(ip net.IP) bool {
+	return ip.To4() == nil
+}
+
+// AddressesRouting takes a slice of Virtual IPs and returns a configured address in the current network namespace that directly routes to at least one of those vips. If the interface containing that address is dual-stack, it will also return a single address of the opposite IP family. You can optionally pass an AddressFilter to further filter down which addresses are considered
 func AddressesRouting(vips []net.IP, af AddressFilter) ([]net.IP, error) {
 	return addressesRoutingInternal(vips, af, getAddrs, getRouteMap)
 }
@@ -128,6 +132,7 @@ func addressesRoutingInternal(vips []net.IP, af AddressFilter, getAddrs addressM
 	var routeMap map[int][]netlink.Route
 	matches := make([]net.IP, 0)
 	for link, addresses := range addrMap {
+	addrLoop:
 		for _, address := range addresses {
 			maskPrefix, maskBits := address.Mask.Size()
 			if net.IPv6len == len(address.IP) && maskPrefix == maskBits {
@@ -146,6 +151,7 @@ func addressesRoutingInternal(vips []net.IP, af AddressFilter, getAddrs addressM
 							if containmentNet.Contains(vip) {
 								log.Infof("Address %s with route %s contains VIP %s", address, route, vip)
 								matches = append(matches, address.IP)
+								break addrLoop
 							}
 						}
 					}
@@ -156,11 +162,22 @@ func addressesRoutingInternal(vips []net.IP, af AddressFilter, getAddrs addressM
 					if address.Contains(vip) {
 						log.Infof("Address %s contains VIP %s", address, vip)
 						matches = append(matches, address.IP)
+						break addrLoop
 					}
 				}
 			}
 		}
 
+		if len(matches) > 0 {
+			// Find an address of the opposite IP family on the same interface
+			for _, address := range addresses {
+				if isIPv6(address.IP) != isIPv6(matches[0]) {
+					matches = append(matches, address.IP)
+					break
+				}
+			}
+			break
+		}
 	}
 	return matches, nil
 }
@@ -170,7 +187,7 @@ func defaultRoute(route netlink.Route) bool {
 	return route.Dst == nil
 }
 
-// AddressesDefault and returns a slice of configured addresses in the current network namespace associated with default routes. You can optionally pass an AddressFilter to further filter down which addresses are considered
+// AddressesDefault returns a slice of configured addresses in the current network namespace associated with default routes; IPv4 first (if any), then IPv6 (if any). You can optionally pass an AddressFilter to further filter down which addresses are considered
 func AddressesDefault(af AddressFilter) ([]net.IP, error) {
 	return addressesDefaultInternal(af, getAddrs, getRouteMap)
 }
@@ -191,6 +208,10 @@ func addressesDefaultInternal(af AddressFilter, getAddrs addressMapFunc, getRout
 			continue
 		}
 		for _, address := range addresses {
+			if len(matches) > 0 && (isIPv6(address.IP) == isIPv6(matches[0])) {
+				continue
+			}
+
 			log.Infof("Address %s is on interface %s with default route", address, link.Attrs().Name)
 			matches = append(matches, address.IP)
 		}

pkg/utils/addresses_test.go
@@ -161,7 +161,7 @@ var _ = Describe("addresses", func() {
 			ipv4RouteMap,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("10.0.0.100")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5")}))
 	})
 
 	It("matches an IPv4 VIP on the secondary interface", func() {
@@ -183,7 +183,7 @@ var _ = Describe("addresses", func() {
 			ipv4RouteMapDefaultEth1,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("10.0.0.100")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5")}))
 	})
 
 	It("matches an IPv6 VIP on the primary interface", func() {
@@ -216,7 +216,7 @@ var _ = Describe("addresses", func() {
 			dualStackRouteMap,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("10.0.0.100")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("fd00::5")}))
 	})
 
 	It("matches an IPv6 VIP on a dual-stack interface", func() {
@@ -227,7 +227,7 @@ var _ = Describe("addresses", func() {
 			dualStackRouteMap,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("fd01::5")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("fd01::5"), net.ParseIP("192.168.1.2")}))
 	})
 
 	It("finds an interface with a default route in an IPv4 cluster", func() {
@@ -237,7 +237,7 @@ var _ = Describe("addresses", func() {
 			ipv4RouteMap,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("10.0.0.100")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5")}))
 	})
 
 	It("finds an interface with a default route when that's not the first interface", func() {
@@ -267,7 +267,7 @@ var _ = Describe("addresses", func() {
 			dualStackRouteMap,
 		)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("10.0.0.100"), net.ParseIP("fd00::5")}))
+		Expect(addrs).To(Equal([]net.IP{net.ParseIP("10.0.0.5"), net.ParseIP("fd00::5")}))
 	})
 })
 

commit dc0531e62a8328710974a8f6738d848f79cad643

Bug 1894539: Allow node-ip to function without attachment to VIP network
Fall back to IP addresses associated with the default route in the when
the node is not on a subnet containing any of the VIPs.

Restore the feature implemented in
384c50336098baf83cb66c823cf142408d5ba8cf and broken with 83d05afd1b76b8f0078887f1001e722284235a3a.

cmd/runtimecfg/node-ip.go
@@ -144,7 +144,8 @@ func getSuitableIPs(retry bool, vips []net.IP) (chosen []net.IP, err error) {
 			if len(chosen) > 0 || err != nil {
 				return chosen, err
 			}
-		} else {
+		}
+		if len(chosen) == 0 {
 			chosen, err = utils.AddressesDefault(utils.ValidNodeAddress)
 			if len(chosen) > 0 || err != nil {
 				return chosen, err