7352e6feae1755066403fc4f9740ed0587cbbc32
Yossi Boaron <yboaron@redhat.com>
2021-01-31 17:02:42 +0200

    Add CloseIdleConnections for HTTP K8S API healthcheck
    We noticed a linearly growing number of TCP connections in haproxy in all clusters,
    that are caused by periodic K8S API health check calls by the haproxy-monitor.
    This PR verifies that unneeded HTTP connections being closed.

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -80,6 +80,7 @@ func IsKubernetesHealthy(port uint16) (bool, error) {
 	if err != nil {
 		return false, err
 	}
+	defer client.CloseIdleConnections()
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
