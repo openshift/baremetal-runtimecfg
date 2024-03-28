391bf2d8856a1f3920f54e4e67ec43e704706d2a
Sandhya Dasu <sdasu@redhat.com>
2024-03-28 13:45:00 -0400

    Vendoring updates due to updated go.mod

--- vendor/github.com/openshift/installer/pkg/types/openstack/machinepool.go
+++ vendor/github.com/openshift/installer/pkg/types/openstack/machinepool.go
@@ -1,14 +1,45 @@
 package openstack
 
+import machinev1alpha1 "github.com/openshift/api/machine/v1alpha1"
+
 // MachinePool stores the configuration for a machine pool installed
 // on OpenStack.
 type MachinePool struct {
 	// FlavorName defines the OpenStack Nova flavor.
 	// eg. m1.large
 	FlavorName string `json:"type"`
+
+	// RootVolume defines the root volume for instances in the machine pool.
+	// The instances use ephemeral disks if not set.
+	// +optional
+	RootVolume *RootVolume `json:"rootVolume,omitempty"`
+
+	// AdditionalNetworkIDs contains IDs of additional networks for machines,
+	// where each ID is presented in UUID v4 format.
+	// Allowed address pairs won't be created for the additional networks.
+	// +optional
+	AdditionalNetworkIDs []string `json:"additionalNetworkIDs,omitempty"`
+
+	// AdditionalSecurityGroupIDs contains IDs of additional security groups for machines,
+	// where each ID is presented in UUID v4 format.
+	// +optional
+	AdditionalSecurityGroupIDs []string `json:"additionalSecurityGroupIDs,omitempty"`
+
+	// ServerGroupPolicy will be used to create the Server Group that will contain all the machines of this MachinePool.
+	// Defaults to "soft-anti-affinity".
+	ServerGroupPolicy ServerGroupPolicy `json:"serverGroupPolicy,omitempty"`
+
+	// Zones is the list of availability zones where the instances should be deployed.
+	// If no zones are provided, all instances will be deployed on OpenStack Nova default availability zone
+	// +optional
+	Zones []string `json:"zones,omitempty"`
+
+	// FailureDomains is a Tech Preview feature for resource placement. It
+	// is incompatible with zones and rootVolume zones.
+	FailureDomains []FailureDomain `json:"failureDomains,omitempty"`
 }
 
-// Set sets the values from `required` to `a`.
+// Set sets the values from `required` to `o`.
 func (o *MachinePool) Set(required *MachinePool) {
 	if required == nil || o == nil {
 		return
@@ -17,4 +48,103 @@ func (o *MachinePool) Set(required *MachinePool) {
 	if required.FlavorName != "" {
 		o.FlavorName = required.FlavorName
 	}
+
+	if required.RootVolume != nil {
+		if o.RootVolume == nil {
+			o.RootVolume = new(RootVolume)
+		}
+		o.RootVolume.Size = required.RootVolume.Size
+		o.RootVolume.Type = required.RootVolume.Type
+		if len(required.RootVolume.Zones) > 0 {
+			o.RootVolume.Zones = required.RootVolume.Zones
+		}
+	}
+
+	if required.AdditionalNetworkIDs != nil {
+		o.AdditionalNetworkIDs = append(required.AdditionalNetworkIDs[:0:0], required.AdditionalNetworkIDs...)
+	}
+
+	if required.AdditionalSecurityGroupIDs != nil {
+		o.AdditionalSecurityGroupIDs = append(required.AdditionalSecurityGroupIDs[:0:0], required.AdditionalSecurityGroupIDs...)
+	}
+
+	if required.ServerGroupPolicy != "" {
+		o.ServerGroupPolicy = required.ServerGroupPolicy
+	}
+
+	if len(required.Zones) > 0 {
+		o.Zones = required.Zones
+	}
+
+	if len(required.FailureDomains) > 0 {
+		o.FailureDomains = required.FailureDomains
+	}
+}
+
+// RootVolume defines the storage for an instance.
+type RootVolume struct {
+	// Size defines the size of the volume in gibibytes (GiB).
+	// Required
+	Size int `json:"size"`
+	// Type defines the type of the volume.
+	// Required
+	Type string `json:"type"`
+
+	// Zones is the list of availability zones where the root volumes should be deployed.
+	// If no zones are provided, all instances will be deployed on OpenStack Cinder default availability zone
+	// +optional
+	Zones []string `json:"zones,omitempty"`
+}
+
+// -=**
+// FailureDomain and its types are part of a Tech Preview feature, and may change before they're moved to openshift/api
+// **=-
+
+// FailureDomain defines a set of correlated fault domains across different
+// OpenStack services: compute, storage, and network.
+type FailureDomain struct {
+	// ComputeAvailabilityZone is the name of a valid nova availability zone. The server will be created in this availability zone.
+	// +optional
+	ComputeAvailabilityZone string `json:"computeAvailabilityZone"`
+
+	// StorageAvailabilityZone is the name of a valid cinder availability
+	// zone. This will be the availability zone of the root volume if one is
+	// specified.
+	// +optional
+	StorageAvailabilityZone string `json:"storageAvailabilityZone"`
+
+	// Ports defines a set of port targets which can be referenced by machines in this failure domain.
+	//
+	// +optional
+	PortTargets []NamedPortTarget `json:"portTargets"`
+}
+
+// NamedPortTarget includes an ID to characterize a PortTarget with its
+// intended purpose. If ID is set to "control-plane", then the PortTarget will
+// replace the default cluster primary network (or the machinesSubnet if
+// defined) as the first network for the machine.
+type NamedPortTarget struct {
+	ID         string `json:"id"`
+	PortTarget `json:",inline"`
+}
+
+// PortTarget defines, directly or indirectly, one or more subnets where to attach a port.
+type PortTarget struct {
+	// Network is a query for an openstack network that the port will be created or discovered on.
+	// This will fail if the query returns more than one network.
+	Network NetworkFilter `json:"network,omitempty"`
+	// Specify pairs of subnet and/or IP address. These should be subnets of the network with the given NetworkID.
+	FixedIPs []FixedIP `json:"fixedIPs,omitempty"`
+}
+
+// NetworkFilter defines a network either by name or by ID.
+type NetworkFilter struct {
+	Name string `json:"name,omitempty"`
+	ID   string `json:"id,omitempty"`
+}
+
+// FixedIP defines a subnet.
+type FixedIP struct {
+	// subnetID specifies the ID of the subnet where the fixed IP will be allocated.
+	Subnet machinev1alpha1.SubnetFilter `json:"subnet"`
 }
--- vendor/github.com/openshift/installer/pkg/types/openstack/platform.go
+++ vendor/github.com/openshift/installer/pkg/types/openstack/platform.go
@@ -1,10 +1,16 @@
 package openstack
 
+import (
+	configv1 "github.com/openshift/api/config/v1"
+)
+
 // Platform stores all the global configuration that all
 // machinesets use.
 type Platform struct {
 	// Region specifies the OpenStack region where the cluster will be created.
-	Region string `json:"region"`
+	// Deprecated: this value is not used by the installer.
+	// +optional
+	DeprecatedRegion string `json:"region,omitempty"`
 
 	// DefaultMachinePlatform is the default configuration used when
 	// installing on OpenStack for machine pools which do not define their own
@@ -12,23 +18,109 @@ type Platform struct {
 	// +optional
 	DefaultMachinePlatform *MachinePool `json:"defaultMachinePlatform,omitempty"`
 
-	// Cloud
-	// Name of OpenStack cloud to use from clouds.yaml
+	// Cloud is the name of OpenStack cloud to use from clouds.yaml.
 	Cloud string `json:"cloud"`
 
-	// ExternalNetwork
-	// The OpenStack external network name to be used for installation.
-	ExternalNetwork string `json:"externalNetwork"`
+	// ExternalNetwork is name of the external network in your OpenStack cluster.
+	// +optional
+	ExternalNetwork string `json:"externalNetwork,omitempty"`
 
-	// FlavorName
-	// The OpenStack compute flavor to use for servers.
-	FlavorName string `json:"computeFlavor"`
+	// DeprecatedFlavorName is the name of the flavor to use for instances in this cluster.
+	// Deprecated: use FlavorName in DefaultMachinePlatform to define default flavor.
+	// +optional
+	DeprecatedFlavorName string `json:"computeFlavor,omitempty"`
 
-	// LbFloatingIP
-	// Existing Floating IP to associate with the OpenStack load balancer.
-	LbFloatingIP string `json:"lbFloatingIP"`
+	// LbFloatingIP is the IP address of an available floating IP in your OpenStack cluster
+	// to associate with the OpenShift load balancer.
+	// Deprecated: this value has been renamed to apiFloatingIP.
+	// +optional
+	DeprecatedLbFloatingIP string `json:"lbFloatingIP,omitempty"`
 
-	// TrunkSupport
-	// Whether OpenStack ports can be trunked
-	TrunkSupport string `json:"trunkSupport"`
+	// APIFloatingIP is the IP address of an available floating IP in your OpenStack cluster
+	// to associate with the OpenShift API load balancer.
+	// +optional
+	APIFloatingIP string `json:"apiFloatingIP,omitempty"`
+
+	// IngressFloatingIP is the ID of an available floating IP in your OpenStack cluster
+	// that will be associated with the OpenShift ingress port
+	// +optional
+	IngressFloatingIP string `json:"ingressFloatingIP,omitempty"`
+
+	// ExternalDNS holds the IP addresses of dns servers that will
+	// be added to the dns resolution of all instances in the cluster.
+	// +optional
+	ExternalDNS []string `json:"externalDNS"`
+
+	// TrunkSupport holds a `0` or `1` value that indicates whether or not to use trunk ports
+	// in your OpenShift cluster.
+	// Deprecated: this value is set by the installer automatically.
+	// +optional
+	DeprecatedTrunkSupport string `json:"trunkSupport,omitempty"`
+
+	// OctaviaSupport holds a `0` or `1` value that indicates whether your OpenStack
+	// cluster supports Octavia Loadbalancing.
+	// Deprecated: this value is set by the installer automatically.
+	// +optional
+	DeprecatedOctaviaSupport string `json:"octaviaSupport,omitempty"`
+
+	// ClusterOSImage is either a URL with `http(s)` or `file` scheme to override
+	// the default OS image for cluster nodes, or an existing Glance image name.
+	// +optional
+	ClusterOSImage string `json:"clusterOSImage,omitempty"`
+
+	// ClusterOSImageProperties is a list of properties to be added to the metadata of the uploaded Glance ClusterOSImage.
+	// Default: the default is to not set any properties.
+	// +optional
+	ClusterOSImageProperties map[string]string `json:"clusterOSImageProperties,omitempty"`
+
+	// DeprecatedAPIVIP is the static IP on the nodes subnet that the api port for openshift will be assigned
+	// Default: will be set to the 5 on the first entry in the machineNetwork CIDR
+	// Deprecated: Use APIVIPs
+	//
+	// +kubebuilder:validation:Format=ip
+	// +optional
+	DeprecatedAPIVIP string `json:"apiVIP,omitempty"`
+
+	// APIVIPs contains the VIP(s) on the nodes subnet that the api port for
+	// openshift will be assigned. In dual stack clusters it contains an IPv4
+	// and IPv6 address, otherwise only one VIP
+	// Default: will be set to the 5 on the first entry in the machineNetwork
+	// CIDR
+	//
+	// +kubebuilder:validation:MaxItems=2
+	// +kubebuilder:validation:UniqueItems=true
+	// +kubebuilder:validation:Format=ip
+	// +optional
+	APIVIPs []string `json:"apiVIPs,omitempty"`
+
+	// DeprecatedIngressVIP is the static IP on the nodes subnet that the apps port for openshift will be assigned
+	// Default: will be set to the 7 on the first entry in the machineNetwork CIDR
+	// Deprecated: Use IngressVIPs
+	//
+	// +kubebuilder:validation:Format=ip
+	// +optional
+	DeprecatedIngressVIP string `json:"ingressVIP,omitempty"`
+
+	// IngressVIPs contains the VIP(s) on the nodes subnet that the apps port
+	// for openshift will be assigned. In dual stack clusters it contains an
+	// IPv4 and IPv6 address, otherwise only one VIP
+	// Default: will be set to the 7 on the first entry in the machineNetwork
+	// CIDR
+	//
+	// +kubebuilder:validation:MaxItems=2
+	// +kubebuilder:validation:UniqueItems=true
+	// +kubebuilder:validation:Format=ip
+	// +optional
+	IngressVIPs []string `json:"ingressVIPs,omitempty"`
+
+	// MachinesSubnet is the UUIDv4 of an openstack subnet. This subnet will be used by all nodes created by the installer.
+	// By setting this, the installer will no longer create a network and subnet.
+	// The subnet and network specified in MachinesSubnet will not be deleted or modified by the installer.
+	// +optional
+	MachinesSubnet string `json:"machinesSubnet,omitempty"`
+
+	// LoadBalancer defines how the load balancer used by the cluster is configured.
+	// LoadBalancer is available in TechPreview.
+	// +optional
+	LoadBalancer *configv1.OpenStackPlatformLoadBalancer `json:"loadBalancer,omitempty"`
 }
